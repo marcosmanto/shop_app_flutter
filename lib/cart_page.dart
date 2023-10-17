227279d7c3429d8028636a74783b0fc72f473668
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-10-16 22:37:44 -0300

    Added empty cart display message.

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -13,41 +13,49 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cart'),
       ),
-      body: ListView.builder(
-        itemCount: cart.length,
-        itemBuilder: (BuildContext context, int index) {
-          final cartItem = cart[index];
-          return ListTile(
-            leading: Container(
-              width: 60.0,
-              height: 100.0,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  fit: BoxFit.fitWidth, // This is the scale you're looking for
-                  image: AssetImage(cartItem['imageUrl'].toString()),
-                ),
+      body: cart.isEmpty
+          ? Center(
+              child: Text(
+                'Empty cart',
+                style: Theme.of(context).textTheme.bodySmall,
               ),
-            ),
-            /*leading: CircleAvatar(
+            )
+          : ListView.builder(
+              itemCount: cart.length,
+              itemBuilder: (BuildContext context, int index) {
+                final cartItem = cart[index];
+                return ListTile(
+                  leading: Container(
+                    width: 60.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit
+                            .fitWidth, // This is the scale you're looking for
+                        image: AssetImage(cartItem['imageUrl'].toString()),
+                      ),
+                    ),
+                  ),
+                  /*leading: CircleAvatar(
               radius: 32,
               backgroundImage: AssetImage(cartItem['imageUrl'].toString()),
             ),*/
-            trailing: IconButton(
-                onPressed: () =>
-                    Provider.of<CartProvider>(context, listen: false)
-                        .removeProduct(cartItem),
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                )),
-            title: Text(cartItem['title'].toString(),
-                style: Theme.of(context).textTheme.bodySmall),
-            subtitle: Text('Size: ${cartItem['size']}'),
-          );
-        },
-      ),
+                  trailing: IconButton(
+                      onPressed: () =>
+                          Provider.of<CartProvider>(context, listen: false)
+                              .removeProduct(cartItem),
+                      icon: Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      )),
+                  title: Text(cartItem['title'].toString(),
+                      style: Theme.of(context).textTheme.bodySmall),
+                  subtitle: Text('Size: ${cartItem['size']}'),
+                );
+              },
+            ),
     );
   }
 }
