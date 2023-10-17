fcbc76e88822daa77b1d3a9f2bf5cc1033c457df
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-10-16 21:28:09 -0300

    Fisnished cart page

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'global_variables.dart';
+
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Cart Page'),
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (BuildContext context, int index) {
+          final cartItem = cart[index];
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 32,
+              backgroundImage: AssetImage(cartItem['imageUrl'].toString()),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                )),
+            title: Text(cartItem['title'].toString(),
+                style: Theme.of(context).textTheme.bodySmall),
+            subtitle: Text('Size: ${cartItem['size']}'),
+          );
+        },
       ),
     );
   }
